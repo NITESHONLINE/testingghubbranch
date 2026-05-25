cf44bdb04d469db4902299d42239fd7df46412ef
NITESHONLINE <niteshonline8@gmail.com>
2026-05-25 15:44:19 +0545

    product page created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
+import Product from "./pages/Product";
 
 // alt + shift + o => remove unwanted imports
 
@@ -11,6 +12,7 @@ function App() {
 
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/product" element={<Product/>} />
       <Route path="/contact" element={<Contact/>} />
     </Routes>
     
